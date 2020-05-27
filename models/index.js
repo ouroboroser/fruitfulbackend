9ce72f20c186ffe1107a0bd86c840764ccdc3d55
Zhenya <evgeniyagorshchyk@gmail.com>
2020-05-27 18:54:54 +0300

    fix

--- models/index.js
+++ models/index.js
@@ -9,7 +9,7 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 const http = require('http');
 
-// let sequelize;
+let sequelize;
 // if (config.use_env_variable) {
 //   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 // } else {
