6d7e11bde3e2a69d59645ab572f6a77d2066096d
Zhenya <evgeniyagorshchyk@gmail.com>
2020-05-27 19:32:52 +0300

    f

--- models/index.js
+++ models/index.js
@@ -1,54 +1,87 @@
-'use strict';
+// 'use strict';
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
-const http = require('http');
+// const fs = require('fs');
+// const path = require('path');
+// const Sequelize = require('sequelize');
+// const basename = path.basename(__filename);
+// const env = process.env.NODE_ENV || 'development';
+// const config = require(__dirname + '/../config/config.json')[env];
+// const db = {};
+// const http = require('http');
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
-// checks if env is Heroku, if so, sets sequelize to utilize the database hosted on heroku
-// if (process.env.DATABASE_URL) {
-//   // the application is executed on Heroku ... use the postgres database
-//   sequelize = new Sequelize(process.env.DATABASE_URL, {
-//     dialect:  'postgres',
-//     protocol: 'postgres',
-//     ssl:true
-//   })
+// let sequelize;
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
 // }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
-  });
+// // checks if env is Heroku, if so, sets sequelize to utilize the database hosted on heroku
+// // if (process.env.DATABASE_URL) {
+// //   // the application is executed on Heroku ... use the postgres database
+// //   sequelize = new Sequelize(process.env.DATABASE_URL, {
+// //     dialect:  'postgres',
+// //     protocol: 'postgres',
+// //     ssl:true
+// //   })
+// // }
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-// db.sequelize.sync().then(function() {
-//   http.createServer(app).listen(app.get('port'), function(){
-//     console.log('Express server listening on port ' + app.get('port'));
+// fs
+//   .readdirSync(__dirname)
+//   .filter(file => {
+//     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+//   })
+//   .forEach(file => {
+//     const model = sequelize['import'](path.join(__dirname, file));
+//     db[model.name] = model;
 //   });
+
+// Object.keys(db).forEach(modelName => {
+//   if (db[modelName].associate) {
+//     db[modelName].associate(db);
+//   }
 // });
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+// // db.sequelize.sync().then(function() {
+// //   http.createServer(app).listen(app.get('port'), function(){
+// //     console.log('Express server listening on port ' + app.get('port'));
+// //   });
+// // });
 
-module.exports = db;
+// db.sequelize = sequelize;
+// db.Sequelize = Sequelize;
+
+// module.exports = db;
+
+if (!global.hasOwnProperty('db')) {
+  var Sequelize = require('sequelize')
+    , sequelize = null
+
+  if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
+    // the application is executed on Heroku ... use the postgres database
+    sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
+      dialect:  'postgres',
+      protocol: 'postgres',
+      port:     match[4],
+      host:     match[3],
+      logging:  true //false
+    })
+  } else {
+    // the application is executed on the local machine ... use mysql
+    sequelize = new Sequelize('example-app-db', 'root', null)
+  }
+
+  global.db = {
+    Sequelize: Sequelize,
+    sequelize: sequelize,
+    User:      sequelize.import(__dirname + '/user') 
+    // add your other models here
+  }
+
+  /*
+    Associations can be defined here. E.g. like this:
+    global.db.User.hasMany(global.db.SomethingElse)
+  */
+}
+
+module.exports = global.db
