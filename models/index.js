ffa7be24d872bf20f59b2a3f0654d9b13564c19d
Zhenya <evgeniyagorshchyk@gmail.com>
2020-05-27 19:06:44 +0300

    fix

--- models/index.js
+++ models/index.js
@@ -21,7 +21,8 @@ if (process.env.DATABASE_URL) {
   // the application is executed on Heroku ... use the postgres database
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect:  'postgres',
-    protocol: 'postgres'
+    protocol: 'postgres',
+    ssl:true
   })
 }
 
